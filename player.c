e8a821bb4330f15b574a355a1f5f4d425c2897ab
Autier Agathe <aautier@2D-LINUX-044.insa-lyon.fr>
2023-11-22 09:03:28 +0100

    methodes de bases player

--- player.c
+++ player.c
@@ -4,18 +4,35 @@
 // Définition de la structure représentant la "classe"
 typedef struct {
     // Déclaration des membres de la classe
-    char* Pseudo;
+    char* pseudo;
+    char* password;
     char* bio;
     int state;
-    List friends;
+    List* friends;
     
 } Player;
 
 // Déclaration des fonctions associées à la "classe"
-Player* creer(char* pseudo){
+Player* creer(char* name, char* pw){
+
+    Player* p;
+    p->pseudo=name;
+    p->password=pw;
+    p->bio="";
+    p->state=0;
+    p->friends=List.createList();
     
 }
-void Player_detruire(Player* objet);
-void Player_afficher(Player* objet);
+void Player_detruire(Player* objet){
+    free(Player->pseudo);
+    free(Player->bio);
+    free(Player->pw);
+    free(Player->friends)
+    free(Player);
+
+}
+void Player_afficher(Player* objet){
+    printf("pseudo : %s\npw : %s\nbio : %s\nstate : %i\n",Player->pseudo,Player->password,Player->bio,Player->state);
+}
 
 #endif  // PLAYER_H
